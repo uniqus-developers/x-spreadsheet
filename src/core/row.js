e5c545d9fc87f03fbfec6b7764f51be4aef5ebae
akash-uniqus <akashmishra@uniqus.com>
2025-04-10 19:42:16 +0530

    Fix: Initialize cellMeta property for cloned cells in row handling

--- src/core/row.js
+++ src/core/row.js
@@ -182,6 +182,7 @@ class Rows {
                 const nri = ii + (i - sri);
                 const nci = jj + (j - sci);
                 const ncell = helper.cloneDeep(this._[i].cells[j]);
+                ncell.cellMeta = {};
                 // ncell.text
                 if (autofill && ncell && ncell.text && ncell.text.length > 0) {
                   const { text } = ncell;
@@ -235,6 +236,7 @@ class Rows {
           nci = dstCellRange.sci + (nci - srcCellRange.sci);
         }
         ncellmm[nri] = ncellmm[nri] || { cells: {} };
+        this._[ri].cells[ci].cellMeta = {};
         ncellmm[nri].cells[nci] = this._[ri].cells[ci];
       });
     });
